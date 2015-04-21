bbffbc52ed1530023f5ea63ceb89975e50ae8886
Cody Bouscaren & Kevin Chen <student@epicodus.com>
2015-04-21 16:46:53 -0700

    Separate Approach (root + suffix)

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,24 +1,70 @@
 class Fixnum
     define_method(:numbers_to_words) do
-        ones = {'1' => "one", '2' => "two", '3' => "three"}
-        tens = {'2' => 'twenty', '3' => 'thirty'}
-        hundreds = {'1' => 'one hundred', '2' => 'two hundred'}
-        number = self
-        num_array = number.to_s.reverse.split("")
-        num_array_size = num_array.length
-        written_nums = [ ]
-        if num_array_size == 1
-            written_nums.unshift(ones.fetch(num_array[0]))
-        elsif num_array_size == 2
-            written_nums.unshift(ones.fetch(num_array[0]))
-            written_nums.unshift(tens.fetch(num_array[1]))
-        elsif num_array_size == 3
-            written_nums.unshift(ones.fetch(num_array[0]))
-            written_nums.unshift(tens.fetch(num_array[1]))
-            written_nums.unshift(hundreds.fetch(num_array[2]))
+        my_number = self
+        numbers_array = my_number.to_s.split('')
+        array_size = numbers_array.length
+
+        digits = {
+            '0' => '',
+            '1' => 'one',
+            '2' => 'two',
+            '3' => 'three',
+            '4' => 'four',
+            '5' => 'five',
+            '6' => 'six',
+            '7' => 'seven',
+            '8' => 'eight',
+            '9' => 'nine'
+        }
+        teens_digits = {
+            '0' => 'ten',
+            '1' => 'eleven',
+            '2' => 'twelve',
+            '3' => 'thirteen',
+            '4' => 'fourteen',
+            '5' => 'fifteen',
+            '6' => 'sixteen',
+            '7' => 'seventeen',
+            '8' => 'eighteen',
+            '9' => 'nineteen'
+        }
+        special_digits = {
+            '0' => '',
+            '1' => '',
+            '2' => 'twen',
+            '3' => 'thir'
+        }
+        suffix = {
+            0 => '',
+            1 => 'ty',
+            2 => 'hundred',
+            3 => 'thousand',
+            4 => ''
+        }
+        to_word = []
+        array_size.times() do |time|
+            current_number = numbers_array[time]
+            number_of_followers = array_size - (time + 1)
+            digit_word = ""
+            digit_suffix = ""
+            if number_of_followers == 1
+                if current_number == '1'
+                    next_number = numbers_array[time + 1]
+                    digit_word = teens_digits.fetch(next_number)
+                    break
+                else
+                    digit_word = special_digits.fetch(current_number)
+                    digit_suffix = suffix.fetch(number_of_followers)
+                    to_word.push(digit_word + digit_suffix)
+                end
+            else
+                digit_word = digits.fetch(current_number)
+                digit_suffix = suffix.fetch(number_of_followers)
+                to_word.push(digit_word + " " + digit_suffix)
+            end
         end
-        written_form = written_nums.join(" ")
-        puts written_form
-        return written_form
+        word = to_word.join(" ").strip()
+        puts word
+        word
     end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -6,9 +6,12 @@ describe ('Fixnum#numbers_to_words') do
         expect(2.numbers_to_words()).to(eq("two"))
     end
     it("takes a two digit number and returns the written form of that number") do
-        expect(23.numbers_to_words()).to(eq("twenty three"))
+        expect(33.numbers_to_words()).to(eq("thirty three"))
     end
     it("takes a three digit number and returns the written form of that number") do
         expect(123.numbers_to_words()).to(eq("one hundred twenty three"))
+    end
+    it("takes a 4 digit number and returns the written form of that number") do
+        expect(1234.numbers_to_words()).to(eq("one thousand two hundred thirty four"))
     end
 end
